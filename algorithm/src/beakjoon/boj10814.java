7600d297bcef2e7a0ab6feee02681314fb5d5b23
myeongheeroh <myeongheeroh@gmail.com>
2021-02-24 23:56:05 +0900

    정렬 : boj10814

---
+++ algorithm/src/beakjoon/boj10814.java
@@ -0,0 +1,37 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj10814 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+		int N = Integer.parseInt(br.readLine());
+		String[][] arr = new String[N][2];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
+		}
+		Arrays.sort(arr,(p1, p2)->{
+			return Integer.parseInt(p1[0]) - Integer.parseInt(p2[0]);
+		});
+		
+		for(int i=0; i<N; i++) {
+			bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+		}
+		
+		bw.close();
+		br.close();
+	}
+	
+}
+
