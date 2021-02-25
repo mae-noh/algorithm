0b8f2d114b62716c902834f5a82ff2a069bf4e96
myeongheeroh <myeongheeroh@gmail.com>
2021-02-25 20:57:09 +0900

    그리디 알고리즘 : boj11047

---
+++ algorithm/src/beakjoon/boj11047.java
@@ -0,0 +1,31 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11047 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int K = Integer.valueOf(st.nextToken());
+		int[] coins = new int[N];
+		for(int i=0; i<N; i++) {
+			coins[i] = Integer.valueOf(br.readLine());
+		}
+		int cnt=0;
+
+		for(int i=N-1; i>=0; i--) {
+			if(K==0) break;
+			if(K-coins[i]<0) 
+			K-=coins[i++];
+			cnt++;
+		}
+		System.out.println(cnt);
+		br.close();
+	}
+	
+}
