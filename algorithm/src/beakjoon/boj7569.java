9ef4e1af8d69c437821c4a313b18a9a0fada64c4
myeongheeroh <myeongheeroh@gmail.com>
2021-03-08 23:43:10 +0900

    BFS DFS : boj7569

---
+++ algorithm/src/beakjoon/boj7569.java
@@ -0,0 +1,19 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj7569 {
+
+	public static int N,M,H;
+	public static int[] dx = {1,0,-1,0};
+	public static int[] dy = {0,1,0,-1};
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	
+}
