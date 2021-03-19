68c809c96df1f3ede76689262fe574eb0d0f8e3b
myeongheeroh <myeongheeroh@gmail.com>
2021-03-19 23:04:33 +0900

    Main

--- algorithm/src/beakjoon/Main.java
+++ algorithm/src/beakjoon/Main.java
@@ -1,50 +1,26 @@
 package beakjoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	
 	   public static void main(String[] args) throws NumberFormatException, IOException{
-		      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		  
+		   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String[] input = br.readLine().split(" ");
+			int N = Integer.parseInt(input[0]);
+			int K = Integer.parseInt(input[1]);
+			input = br.readLine().split(" ");
+			int[] arr = new int[N];
+			for(int i=0; i<N; i++){
+				arr[i] = Integer.parseInt(input[i]);
+			}
+			int result = (int)(Math.abs((N-1.0)/(K-1.0)));
+			System.out.println(result);
+		
+	   }
 
-		      int N = Integer.parseInt(br.readLine());
-		      
-		      long[] length = new long[N-1];
-		      long[] fuelPrice = new long[N];
-		      
-		      StringTokenizer st = new StringTokenizer(br.readLine());
-		      for (int i = 0; i < length.length; i++) {
-		         length[i] = Long.parseLong(st.nextToken());
-		      }
-		      
-		      st = new StringTokenizer(br.readLine());
-		      for (int i = 0; i < fuelPrice.length; i++) {
-		         fuelPrice[i] = Long.parseLong(st.nextToken());
-		      }
-		      
-		      long cost = 0;
-		      long tmpPrice = fuelPrice[0];
-		      for (int i = 0; i < fuelPrice.length-1; i++) {
-		         
-		    	 System.out.println(tmpPrice + " " + length[i]);
-		         cost += tmpPrice * length[i];		         
-		         
-		         if(fuelPrice[i] > fuelPrice[i+1]) {
-		            tmpPrice = fuelPrice[i+1];
-		         }
-		         
-		      }
-		      
-		      bw.write("" + cost);
-		      bw.close();
-		   }
-
-	
 }
 
--- algorithm/src/beakjoon/boj15649.java
+++ algorithm/src/beakjoon/boj15649.java
@@ -15,14 +15,26 @@ public class boj15649 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuffer sb = new StringBuffer();
 		int N = Integer.valueOf(st.nextToken());
+		boolean[] arr = new boolean[N+1];
 		int M = Integer.valueOf(st.nextToken());
-		dfs(N, M, sb);
+		dfs(N, M, sb, arr);
+		bw.write(sb+"");
 		bw.close();
 	}
 
-	private static StringBuffer dfs(int n, int m, StringBuffer sb) {
-		if(m==0) return sb;		
-		return dfs(n, m-1, sb);
+	private static void dfs(int n, int m, StringBuffer sb, boolean[] arr) {
+		if(m==0) {
+			sb.append("\n"); return;
+		}
+		
+		for(int i=1; i<=n; i++) {
+			if(arr[i]) continue;
+			arr[i] = true;
+			sb.append(i).append(" ");
+			dfs(n, m-1, sb, arr);
+			arr[i] = false;
+		}
+		
 	}
 	
 }
