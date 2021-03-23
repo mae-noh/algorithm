a9d2a1c28e9e4a22a60ae8518e9041646922d706
mae <myeongheeroh@gmail.com>
2021-03-23 21:45:51 +0900

    swea : 최빈수구하기

--- algorithm/src/swea/최빈수구하기.java
+++ algorithm/src/swea/최빈수구하기.java
@@ -1,5 +1,12 @@
 package swea;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 최빈수구하기 {
 
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+	
 }
