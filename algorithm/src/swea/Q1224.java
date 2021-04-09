05d302b63af9443822d07453d469a06d23b97d65
mae <myeongheeroh@gmail.com>
2021-04-09 23:43:45 +0900

    List : Q1229

---
+++ algorithm/src/swea/Q1224.java
@@ -0,0 +1,89 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Q1224 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int testCase=1; testCase<=10; testCase++) {
+			br.readLine();
+			String s = br.readLine();
+			System.out.println("#"+testCase+" "+calPostfix(infixToPostfix(s)));
+		}
+	}
+
+	private static int calPostfix(StringBuffer sb) {
+		int answer=0;
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<sb.length(); i++) {
+			char cur = sb.charAt(i);
+			if(cur >= '0' && cur <= '9') {
+				stack.add(cur-'0');
+			}else if(cur == '+') {
+				int second = stack.pop();
+				int first = stack.pop();
+				stack.add(first+second);
+			}else if(cur == '*') {
+				int second = stack.pop();
+				int first = stack.pop();
+				stack.add(first*second);				
+			}
+		}
+		
+		answer = stack.pop();
+		
+		return answer;
+	}
+
+	private static StringBuffer infixToPostfix(String s) {
+		StringBuffer sb = new StringBuffer();
+		Stack<operator> stack = new Stack<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			char cur = s.charAt(i);
+			
+			if(cur == '(') {
+				stack.add(new operator('(', -3));
+			}else if(cur == ')') {
+				while(!stack.isEmpty()) {
+					if(stack.peek().op == '(') {
+						stack.pop();
+						break;
+					}
+					sb.append(stack.pop().op);
+				}
+			}else if(cur >= '0' && cur <= '9') {
+				sb.append(cur);
+			}else if(cur == '+') {
+				while(!stack.isEmpty() && stack.peek().rank >= -2) {
+					sb.append(stack.pop().op);
+				}
+				stack.add(new operator('+',-2));
+			}else if(cur == '*') {
+				while(!stack.isEmpty() && stack.peek().rank >= -1) {
+					sb.append(stack.pop().op);
+				}
+				stack.add(new operator('*',-1));
+			}
+			
+		}
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop().op);
+		}
+		
+		return sb;
+	}
+}
+
+class operator{
+	char op;
+	int rank;
+	operator(char op, int rank){
+		this.op = op;
+		this.rank = rank;
+	}
+}
---
+++ algorithm/src/swea/Q1229.java
@@ -0,0 +1,52 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Q1229 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int testCase=1; testCase<=10; testCase++) {
+			br.readLine();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			List<Integer> arr = new ArrayList<>();
+			while(st.hasMoreTokens()) {
+				arr.add(Integer.parseInt(st.nextToken()));
+			}
+			br.readLine();
+			st = new StringTokenizer(br.readLine());
+			while(st.hasMoreTokens()) {
+				String inst = st.nextToken();
+				System.out.println(inst);
+				if(inst.equals("I")) {
+					int idx = Integer.parseInt(st.nextToken());
+					int num = Integer.parseInt(st.nextToken());
+					System.out.println("*" + idx + " " + num);
+					for(int i=0; i<num; i++) {
+						arr.add(idx++, Integer.parseInt(st.nextToken()));
+					}
+				}
+				if(inst.equals("D")) {
+					int idx = Integer.parseInt(st.nextToken());
+					int num = Integer.parseInt(st.nextToken());
+					for(int i=0; i<num; i++) {
+						arr.remove(idx);
+					}
+				}
+			}
+			bw.write("#"+testCase+" ");
+			for(int i=0; i<10; i++) {
+				bw.write(arr.get(i)+" ");
+			}
+			bw.newLine();
+		}
+		bw.close();
+	}
+}
