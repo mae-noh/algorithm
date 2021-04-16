14a86ec7e00635d32d484f4d505646a32fc54a21
mae <myeongheeroh@gmail.com>
2021-04-16 23:59:29 +0900

    Tree : Q1233

--- algorithm/src/swea/Q1222.java
+++ algorithm/src/swea/Q1222.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Q1222 {
 	
@@ -13,42 +14,49 @@ public class Q1222 {
 		for(int testCase=1; testCase<=10; testCase++) {
 			br.readLine();
 			s = br.readLine();
-			System.out.println("#"+testCase+" "+infixToPostfix(s));
+			String[] st = s.split("");
+			for(String si : st) {
+				System.out.print(si + " ");
+			}
+			//System.out.println("#"+testCase+" "+infixToPostfix(s));
 		}
 	}
 
-	private static int infixToPostfix(String s) {
-		Stack<Character> stack = new Stack<>();
-		StringBuffer sb = new StringBuffer();
-		for(int i=0; i<s.length(); i++) {
-			char op = s.charAt(i);
-			if(op != '+') {
-				sb.append(op);
-			}
-			if(op == '+' && !stack.isEmpty()) {
-				stack.pop();
-				sb.append(op);
-			}
-			if(op == '+' && stack.isEmpty()) stack.add(op);
-		}
-		sb.append(stack.pop());
-		return calPostfix(sb);
-	}
-
-	private static int calPostfix(StringBuffer sb) {
-		Stack<Character> stack = new Stack<>();
-		for(int i=0; i<sb.length(); i++) {
-			char op = sb.charAt(i);
-			if(op != '+') {
-				stack.add((char) (op - '0'));
-			}
-			if(op == '+') {
-				int second = stack.pop();
-				int first = stack.pop();
-				stack.add((char) (first+second));
-			}
-		}
-		return stack.pop();
-	}
-	
 }
+//
+//	private static int infixToPostfix(String s) {
+//		Stack<Character> stack = new Stack<>();
+//		StringBuffer sb = new StringBuffer();
+//		
+//		for(int i=0; i<s.length(); i++) {
+//			char op = s.charAt(i);
+//			if(op != '+') {
+//				sb.append(op);
+//			}
+//			if(op == '+' && !stack.isEmpty()) {
+//				stack.pop();
+//				sb.append(op);
+//			}
+//			if(op == '+' && stack.isEmpty()) stack.add(op);
+//		}
+//		sb.append(stack.pop());
+//		return calPostfix(sb);
+//	}
+//
+//	private static int calPostfix(StringBuffer sb) {
+//		Stack<Character> stack = new Stack<>();
+//		for(int i=0; i<sb.length(); i++) {
+//			char op = sb.charAt(i);
+//			if(op != '+') {
+//				stack.add((char) (op - '0'));
+//			}
+//			if(op == '+') {
+//				int second = stack.pop();
+//				int first = stack.pop();
+//				stack.add((char) (first+second));
+//			}
+//		}
+//		return stack.pop();
+//	}
+//	
+//}
--- algorithm/src/swea/Q1233.java
+++ algorithm/src/swea/Q1233.java
@@ -3,24 +3,31 @@ package swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Q1233 {
 	static String[] tree;
+	static int n;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] st;
+		StringTokenizer st;
 		for(int testCase=1; testCase<=10; testCase++) {
-			int n = Integer.parseInt(br.readLine());
-			tree = new String[n];
+			n = Integer.parseInt(br.readLine());
+			tree = new String[n*2+1];
 			for(int i=1; i<=n; i++) {
-				st = br.readLine().split(" ");
-				tree[i] = st[1];
+				st = new StringTokenizer(br.readLine());
+				tree[i] = st.nextToken();
 			}
-			inOrder(1);
+			isCalculate(tree);
 		}
 	}
-
-	private static boolean inOrder(int d) {
-		//
+	private static boolean isCalculate(String[] tree) {
+		// 왼쪽 오른쪽 자식이 둘다 있어 - 연산자
+		// 왼쪽 오른쪽 자식이 둘다 없어 - 숫자
+		for(int i=1; i<=n; i++) {
+			char ch = tree[i].charAt(0);
+			if(!tree[i*2].equals("0") && !tree[i*2+1].equals("0") && !(ch >= 0 && ch <= 9)) return false;
+		}
+		return false;
 	}
 }
