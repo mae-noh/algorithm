e16d7316685a00c6950ab3e48766042df85c37e6
mae <myeongheeroh@gmail.com>
2021-04-17 14:36:26 +0900

    Tree : Q1233

--- algorithm/src/swea/Q1233.java
+++ algorithm/src/swea/Q1233.java
@@ -3,6 +3,7 @@ package swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q1233 {
@@ -16,18 +17,19 @@ public class Q1233 {
 			tree = new String[n*2+1];
 			for(int i=1; i<=n; i++) {
 				st = new StringTokenizer(br.readLine());
-				tree[i] = st.nextToken();
+				tree[Integer.parseInt(st.nextToken())] = st.nextToken();
 			}
-			isCalculate(tree);
+			System.out.println("#"+testCase+" "+isCalculate(tree));
 		}
 	}
-	private static boolean isCalculate(String[] tree) {
+	private static int isCalculate(String[] tree) {
 		// 왼쪽 오른쪽 자식이 둘다 있어 - 연산자
 		// 왼쪽 오른쪽 자식이 둘다 없어 - 숫자
-		for(int i=1; i<=n; i++) {
+		for(int i=1; i<=n/2+1; i++) {
 			char ch = tree[i].charAt(0);
-			if(!tree[i*2].equals("0") && !tree[i*2+1].equals("0") && !(ch >= 0 && ch <= 9)) return false;
+			if(ch >= '0' && ch <= '9' && (tree[i*2]!=null || tree[i*2+1]!=null)) return 0;
+			if(!(ch >= '0' && ch <= '9') && (tree[i*2]==null || tree[i*2+1]==null)) return 0;
 		}
-		return false;
+		return 1;
 	}
 }
