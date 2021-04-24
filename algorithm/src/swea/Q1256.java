f7f2d96046eafd335eda48997517e929a14b1e9e
mae <myeongheeroh@gmail.com>
2021-04-24 23:47:47 +0900

    String : Q1256

---
+++ algorithm/src/swea/Q1256.java
@@ -0,0 +1,35 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Q1256 {
+
+public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String answer="";
+		br.readLine();
+		for(int testCase=1; testCase<=10; testCase++) {
+			int index = Integer.parseInt(br.readLine());
+			String st = br.readLine();
+			answer = Solution(index, st);
+			System.out.println("#" + testCase + " " + answer);
+		}
+		
+	}
+
+	private static String Solution(int index, String st) {
+		ArrayList<String> list = new ArrayList<>();
+		if(index >= st.length()) return "none";
+		
+		for(int i=0; i<=st.length(); i++) {
+			list.add(st.substring(i, st.length()));
+		}
+		list.sort(null);
+		return list.get(index);
+	}
+	
+}
