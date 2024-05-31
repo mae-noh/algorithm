619a08b95a05523a0f5f8b4f8cba26ce81be5502
mae-noh <myeongheeroh@gmail.com>
2024-05-31 17:36:24 +0900

    leetcode: solution construct quad tree

---
+++ algorithm/leetcode/Solution.java
@@ -0,0 +1,102 @@
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+
+class Solution {
+    // [[0,1],[1,0]]
+    public Node construct(int[][] grid) {
+        Node root = new Node();
+
+        for(int i=0; i<grid.length-1; i++) {
+            // 069
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    
+        // Example 1
+        int[][] grid = {{0,1},{1,0}};
+        Node output1 = solution.construct(grid);
+        System.out.println("Example 1:");
+        System.out.println("length: " + grid.length);
+        System.out.println("Output: " + output1);
+        
+    }
+}
+/*
+Given a n * n matrix grid of 0's and 1's only. We want to represent grid with a Quad-Tree.
+
+Return the root of the Quad-Tree representing grid.
+
+A Quad-Tree is a tree data structure in which each internal node has exactly four children. Besides, each node has two attributes:
+
+val: True if the node represents a grid of 1's or False if the node represents a grid of 0's. Notice that you can assign the val to True or False when isLeaf is False, and both are accepted in the answer.
+isLeaf: True if the node is a leaf node on the tree or False if the node has four children.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+}
+We can construct a Quad-Tree from a two-dimensional area using the following steps:
+
+If the current grid has the same value (i.e all 1's or all 0's) set isLeaf True and set val to the value of the grid and set the four children to Null and stop.
+If the current grid has different values, set isLeaf to False and set val to any value and divide the current grid into four sub-grids as shown in the photo.
+Recurse for each of the children with the proper sub-grid.
+*/
+
+/*
+Input-Output
+
+Input: grid = [[0,1],[1,0]]
+Output: [[0,1],[1,0],[1,1],[1,1],[1,0]]
+Explanation: The explanation of this example is shown below:
+Notice that 0 represents False and 1 represents True in the photo representing the Quad-Tree.
+
+Input: grid = [[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]]
+Output: [[0,1],[1,1],[0,1],[1,1],[1,0],null,null,null,null,[1,0],[1,0],[1,1],[1,1]]
+Explanation: All values in the grid are not the same. We divide the grid into four sub-grids.
+The topLeft, bottomLeft and bottomRight each has the same value.
+The topRight have different values so we divide it into 4 sub-grids where each has the same value.
+Explanation is shown in the photo below:
+
+n == grid.length == grid[i].length
+n == 2x where 0 <= x <= 6
+ */
